67de10000a3698f46b0a597617588f03ca9c2b01
Satyam <satyam.pathak@increff.com>
2023-12-30 02:05:19 +0530

    Added URI to connect to mongodb

--- src/main/java/com/experiment/mongo/dao/BlogDao.java
+++ src/main/java/com/experiment/mongo/dao/BlogDao.java
@@ -1,12 +1,9 @@
 package com.experiment.mongo.dao;
 
-import com.experiment.mongo.entity.document.BlogDocument;
 import com.experiment.mongo.entity.pojo.BlogPojo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogDao extends JpaRepository<BlogPojo, Integer> {
-
 }
